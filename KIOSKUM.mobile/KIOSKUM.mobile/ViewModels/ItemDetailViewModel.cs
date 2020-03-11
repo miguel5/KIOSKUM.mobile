6b150cc94573f2ae8de8798f586d064e4ed8cd47
Miguel <miguelarrepia@gmail.com>
2020-03-11 21:45:16 +0000

    Add project files.

---
+++ KIOSKUM.mobile/KIOSKUM.mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using KIOSKUM.mobile.Models;
+
+namespace KIOSKUM.mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
