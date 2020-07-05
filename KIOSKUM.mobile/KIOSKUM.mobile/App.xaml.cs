10bd2227fddcfe880e7b0c9097d4228298b1af7d
Miguel <miguelarrepia@gmail.com>
2020-07-05 21:45:31 +0100

    the garbage man came by

--- KIOSKUM.mobile/KIOSKUM.mobile/App.xaml.cs
+++ KIOSKUM.mobile/KIOSKUM.mobile/App.xaml.cs
@@ -16,11 +16,6 @@ namespace KIOSKUM.mobile
             DependencyService.Register<MockDataStore>();
             MainPage = new LoginPage();
 
-            AuthToken = new AuthToken{Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwicm9sZSI6IkNsaWVudGUiLCJuYmYiOjE1OTM5MDIyN" +
-                                              "jgsImV4cCI6MTU5NDUwNzA2OCwiaWF0IjoxNTkzOTAyMjY4fQ.PhGr9E9YpGJ9nEJpZe_sw6QVX2GIfy4-2ovr5BsfwCQ" };
-
-            //MainPage = new NavigationPage(new ProdutosPage());  // DEBUG
-            //MainPage = new MainPage();  // DEBUG
         }
 
         protected override void OnStart()
