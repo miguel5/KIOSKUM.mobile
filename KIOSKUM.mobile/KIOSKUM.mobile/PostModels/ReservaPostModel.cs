56e7d8da79d4b6210bacc0b436ca8fd59959f168
Miguel <miguelarrepia@gmail.com>
2020-07-05 04:50:12 +0100

    ReservaPostModel

---
+++ KIOSKUM.mobile/KIOSKUM.mobile/PostModels/ReservaPostModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KIOSKUM.mobile.PostModels
+{
+    public class ReservaPostModel
+    {
+        public List<ReservaItem> Itens { get; set; }
+        public string HoraEntrega { get; set; }
+    }
+
+    public class ReservaItem
+    {
+        public int IdProduto { get; set; }
+        public int Quantidade { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
